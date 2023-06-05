c83658b569cc94d4f1802903dacecff101420aa9
anurag8423 <112267067+anurag8423@users.noreply.github.com>
2023-06-05 15:17:27 +0530

    Add files via upload

---
+++ new/module2.mjs
@@ -0,0 +1,8 @@
+const a="Rohan"
+const b="Harry"
+const c="priyanka"
+const d="Anurag"
+export default a;
+export {d};
+export {b};
+export {c};
---
+++ new/src/components/Alert.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ function Alert(props) {
+    const capitalize=(word)=>{
+        word.toLowerCase();
+        return word.charAt(0).toUpperCase()+word.slice(1);
+    }
+  return (
+    <div style={{height:'50px'}}>
+   { props.alert && <div className={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
+  <strong>{capitalize(props.alert.typ)}</strong> {props.alert.msg}.
+  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>}
+</div>
+  )
+}  
+export default Alert;
+
